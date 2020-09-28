25918a4628a81742e48de93d136871ba6e712999
MakarGlavanar <mr.glavanar@bk.ru>
2020-09-28 23:08:04 +0300

    RIS-16 client: add footer component (#9)

--- client/src/components/common/page/Page.tsx
+++ client/src/components/common/page/Page.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
+import { Footer } from "../footer";
 import { Header } from "../header";
 import styles from "./Page.scss";
 
 export interface IPageProps {
   children: React.ReactNode;
   hasHeader?: boolean;
+  hasFooter?: boolean;
 }
 
-export const Page: React.FC<IPageProps> = ({ children, hasHeader }) => {
+export const Page: React.FC<IPageProps> = ({
+  children,
+  hasHeader,
+  hasFooter,
+}) => {
   return (
     <div className={styles.Page}>
       {hasHeader && <Header />}
       {children}
+      {hasFooter && <Footer />}
     </div>
   );
 };
 
 Page.defaultProps = {
   hasHeader: true,
+  hasFooter: true,
 };
 
 Page.displayName = "Page";
