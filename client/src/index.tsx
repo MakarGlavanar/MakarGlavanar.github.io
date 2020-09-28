daad7ad05550ecdcb929a98cb006d8c80ee523c7
MakarGlavanar <mr.glavanar@bk.ru>
2020-09-28 20:44:38 +0300

    Updates

--- client/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "@scss/index.scss";
-
-const root = document.getElementById("root");
-
-ReactDOM.render(<div>hello</div>, root);
