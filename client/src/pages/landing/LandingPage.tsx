88677638e0b365e90f3e8901f6df45aa59cc84f5
MakarGlavanar <mr.glavanar@bk.ru>
2020-09-29 00:31:59 +0300

    SL-12 client: add cover component (#11)

--- client/src/pages/landing/LandingPage.tsx
+++ client/src/pages/landing/LandingPage.tsx
@@ -1,27 +1,13 @@
 import * as React from "react";
 import { Page } from "@components/common/page";
-import { ISmoothScrollState } from "@providers/smooth-scroll";
-import { Registry } from "@registry/Registry";
+import { Cover } from "@components/cover";
 
 export interface ILandingPageProps {}
 
 export const LandingPage: React.FC<ILandingPageProps> = () => {
-  const smoothScrollHandler = React.useRef((value: ISmoothScrollState) => {
-    console.log(value.scrollTop);
-  });
-
-  React.useEffect(() => {
-    Registry.getSmoothScrollProvider().subscribe(smoothScrollHandler.current);
-
-    return () =>
-      Registry.getSmoothScrollProvider().unsubscribe(
-        smoothScrollHandler.current
-      );
-  });
-
   return (
     <Page>
-      <div style={{ height: "300vh" }}></div>
+      <Cover />
     </Page>
   );
 };
